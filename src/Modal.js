12c9196a724374c34560e5f8357e8f5aa94f419c
Xavier Cambengue <xavierpt35@gmail.com>
2021-09-29 10:36:29 +0100

    Add files via upload

--- src/Modal.js
+++ src/Modal.js
@@ -10,13 +10,13 @@ const Modal = () => {
       }`}
     >
       <div className='modal-content'>
-        <h2>congrats!</h2>
+        <h2>Parabéns!</h2>
         <p>
           You answered {((correct / questions.length) * 100).toFixed(0)}% of
           questions correctly
         </p>
         <button className='close-btn' onClick={closeModal}>
-          play again
+          jogar de novo
         </button>
       </div>
     </div>
--- src/SetupForm.js
+++ src/SetupForm.js
@@ -10,7 +10,7 @@ const SetupForm = () => {
           <h2>setup quiz</h2>
           {/* amount */}
           <div className='form-control'>
-            <label htmlFor='amount'>number of questions</label>
+            <label htmlFor='amount'>número de questões</label>
             <input
               type='number'
               name='amount'
@@ -25,7 +25,7 @@ const SetupForm = () => {
           {/* category */}
 
           <div className='form-control'>
-            <label htmlFor='category'>category</label>
+            <label htmlFor='category'>categoria</label>
             <select
               name='category'
               id='category'
@@ -33,15 +33,15 @@ const SetupForm = () => {
               value={quiz.category}
               onChange={handleChange}
             >
-              <option value='sports'>sports</option>
-              <option value='history'>history</option>
-              <option value='politics'>politics</option>
+              <option value='sports'>Esportes</option>
+              <option value='history'>história</option>
+              <option value='politics'>política</option>
             </select>
           </div>
           {/* difficulty */}
 
           <div className='form-control'>
-            <label htmlFor='difficulty'>select difficulty</label>
+            <label htmlFor='difficulty'>selecione a dificuldade</label>
             <select
               name='difficulty'
               id='difficulty'
@@ -49,18 +49,18 @@ const SetupForm = () => {
               value={quiz.difficulty}
               onChange={handleChange}
             >
-              <option value='easy'>easy</option>
-              <option value='medium'>medium</option>
-              <option value='hard'>hard</option>
+              <option value='easy'>fácil</option>
+              <option value='medium'>média</option>
+              <option value='hard'>dura</option>
             </select>
           </div>
           {error && (
             <p className='error'>
-              can't generate questions, please try different options
+             não pode gerar perguntas, por favor, tente opções diferentes
             </p>
           )}
-          <button type='submit' onClick={handleSubmit} className='submit-btn'>
-            start
+          <button type='submit' onClick={handleSubmit} className='submit-btn'>        
+começar
           </button>
         </form>
       </section>
